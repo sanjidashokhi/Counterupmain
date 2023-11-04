b0387e32f7675fdd39b2cd3fbdd5080173efcf2f
Sanjida_Nasrin_Shokhi <143315231+sanjidashokhi@users.noreply.github.com>
2023-11-04 15:05:10 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// jQuery(document).ready(function($) {
+//     $('.counter').counterUp({
+//         delay: 10,
+//         time: 1000
+//     });
+// });
+
+let counterUp = document.querySelectorAll('.counterUp')
+let arr = Array.from(counterUp)
+
+arr.map((item)=>{
+    // console.log(item)
+    let counter = 0
+
+    function count(){
+        counter ++
+        item.innerHTML = counter
+    
+        if (counter == item.dataset.number){
+            clearInterval(stop)
+        }
+    }
+    let stop = setInterval(function(){
+        count()
+    
+    },1000/item.dataset.number)
+})
+
+
+
+
+
+
+
+
+
+// console.log(counterUp.dataset.number)
+
